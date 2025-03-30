990a3b2c08282e6383e2a9ad82cbac18fff5aecd
ChrisSchlg <2310859012@fh-burgenland>
2025-03-30 18:15:44 +0200

    initial commit

---
+++ src/main/java/at/hochschule/umzug/UmzugBackendApplication.java
@@ -0,0 +1,13 @@
+package at.hochschule.umzug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmzugBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmzugBackendApplication.class, args);
+	}
+
+}
